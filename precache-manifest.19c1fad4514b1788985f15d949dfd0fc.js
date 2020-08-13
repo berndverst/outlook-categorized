e8db75d5281d9344f87df2f37913d274f120b468
Bernd Verst <bverst@gmail.com>
2020-08-13 01:34:39 -0700

    Updates

---
+++ precache-manifest.19c1fad4514b1788985f15d949dfd0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd1aff9c75b8f3c69fa801f098d5465",
+    "url": "/outlook-categorized/index.html"
+  },
+  {
+    "revision": "e8fd62de094bbfa634d6",
+    "url": "/outlook-categorized/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6232d2d092575c67b394",
+    "url": "/outlook-categorized/static/js/2.78437a11.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/outlook-categorized/static/js/2.78437a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fd62de094bbfa634d6",
+    "url": "/outlook-categorized/static/js/main.4195af1c.chunk.js"
+  },
+  {
+    "revision": "97653edf437d27d1fe3b",
+    "url": "/outlook-categorized/static/js/runtime-main.a67cc4a5.js"
+  }
+]);
